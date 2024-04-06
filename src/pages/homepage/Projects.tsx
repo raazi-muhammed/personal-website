31564d34874456ab141fea23b79d384100b3f3ff
Raazi Muhammed K <raazi6163@gmail.com>
2024-04-06 21:43:07 +0530

    feat: add projects

--- src/pages/homepage/Projects.tsx
+++ src/pages/homepage/Projects.tsx
@@ -1,11 +1,157 @@
+import { ChevronRight, Github, Globe } from "lucide-react";
 import Container from "../../components/layout/Components";
 import Heading from "../../components/layout/Heading";
+import {
+    Card,
+    CardHeader,
+    CardBody,
+    CardFooter,
+    Button,
+    Link,
+} from "@nextui-org/react";
 
 export default function Projects() {
+    const projects: {
+        name: string;
+        description: string;
+        date: string;
+        preview: string;
+        links: {
+            name: string;
+            url: string;
+            icon: JSX.Element;
+        }[];
+    }[] = [
+        {
+            name: "Omniflow",
+            description:
+                "Project Toolkit for Effortless Planning and Management of Projects.",
+            date: "Mar 2024 - Present",
+            preview: "/images/homepage/omniflow-preview.png",
+            links: [
+                {
+                    name: "Git repo",
+                    url: "https://github.com/raazi-muhammed/omniflow",
+                    icon: <Github size="1em" />,
+                },
+            ],
+        },
+        {
+            name: "Shop Nexus",
+            description:
+                "A comprehensive Multi-Vendor E-Commerce platform facilitating buying and selling of products",
+            date: "Sep 2023 - Nov 2023",
+            preview: "/images/homepage/omniflow-preview.png",
+            links: [
+                {
+                    name: "Git repo",
+                    url: "https://github.com/raazi-muhammed/shop-nexus",
+                    icon: <Github size="1em" />,
+                },
+                {
+                    name: "Live link",
+                    url: "https://shopnexus.live/",
+                    icon: <Globe size="1em" />,
+                },
+            ],
+        },
+        {
+            name: "Alma Foods",
+            description:
+                "Converted a HTML, CSS, JS website into React + Typescript",
+            date: "Jan 2024 - Jan 2024",
+            preview: "/images/homepage/omniflow-preview.png",
+            links: [],
+        },
+        {
+            name: "Sorting Visualizer",
+            description:
+                "Using this project anyone can explore diverse sorting algorithms like Insertion, Bubble, Selection, Quick, and Merge",
+            date: "Dec 2023 - Dec 2023",
+            preview: "/images/homepage/omniflow-preview.png",
+            links: [
+                {
+                    name: "Git repo",
+                    url: "https://github.com/raazi-muhammed/sorting-visualizer",
+                    icon: <Github size="1em" />,
+                },
+                {
+                    name: "Live link",
+                    url: "https://sorting-visualizer-rmk.netlify.app/",
+                    icon: <Globe size="1em" />,
+                },
+            ],
+        },
+        {
+            name: "Rastreo",
+            description: "A score-tracking app with features like",
+            date: "Mar 2024 - Mar 2024",
+            preview: "/images/homepage/omniflow-preview.png",
+            links: [
+                {
+                    name: "Git repo",
+                    url: "https://github.com/raazi-muhammed/rastreo",
+                    icon: <Github size="1em" />,
+                },
+                {
+                    name: "Live link",
+                    url: "https://rastreo-rmk.netlify.app/",
+                    icon: <Globe size="1em" />,
+                },
+            ],
+        },
+    ];
     return (
         <section className="bg-default-50/50 py-8">
             <Container>
                 <Heading>Projects</Heading>
+                <section className="grid grid-cols-3 gap-4">
+                    {projects.map((project) => (
+                        <Card
+                            isFooterBlurred
+                            className="mb-8 overflow-visible py-4">
+                            <CardHeader className="z-30 flex-col items-start px-4 pb-0 pt-2">
+                                <div className="flex w-full justify-between">
+                                    <p className="text-tiny font-bold uppercase">
+                                        {project.date}
+                                    </p>
+                                    <ChevronRight size="1em" />
+                                </div>
+                                <h4 className="text-large font-bold text-primary">
+                                    {project.name}
+                                </h4>
+                                <small className="text-default-500">
+                                    {project.description}
+                                </small>
+                            </CardHeader>
+                            <CardBody className="-mb-4 -mt-8 overflow-visible p-0 align-bottom">
+                                <div className="absolute bottom-0 left-0 right-0 top-0 z-20 h-full bg-gradient-to-b from-default-50 to-transparent"></div>
+                                <img
+                                    alt="Card background"
+                                    className="z-10 m-0 mb-0 mt-auto h-full rounded-xl object-cover"
+                                    src={project.preview}
+                                />
+                            </CardBody>
+                            {project.links.length ? (
+                                <CardFooter className="absolute -bottom-5 left-4 right-4 z-50 mx-auto h-10 w-fit justify-between gap-2 rounded-large border-1 border-white/10 shadow-small before:rounded-xl before:bg-white/10">
+                                    {project.links.map((link) => (
+                                        <Button
+                                            href={link.url}
+                                            as={Link}
+                                            className="bg-white/10 text-tiny text-white"
+                                            variant="flat"
+                                            color="default"
+                                            radius="lg"
+                                            size="sm">
+                                            {link.icon}
+                                            {link.name}
+                                        </Button>
+                                    ))}
+                                </CardFooter>
+                            ) : null}
+                        </Card>
+                    ))}
+                </section>
             </Container>
         </section>
     );
