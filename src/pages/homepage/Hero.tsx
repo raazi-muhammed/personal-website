4de760f614e792f3c2a01915f37a7bf6088bfae5
Raazi Muhammed K <raazi6163@gmail.com>
2024-04-07 15:04:06 +0530

    feat: updated hero section

--- src/pages/homepage/Hero.tsx
+++ src/pages/homepage/Hero.tsx
@@ -1,16 +1,30 @@
 import Container from "../../components/layout/Components";
 
+import { motion } from "framer-motion";
+
 export default function Hero() {
     return (
-        <div className="grid h-[80svh] place-items-center">
+        <div className="grid place-items-center py-16 text-center">
             <Container>
-                <h1 className="text-5xl font-bold text-primary">
-                    Raazi Muhammed
-                </h1>
-                <p>
-                    Self-Taught Graphic Designer <br />
-                    Turned Self-Taught Web Developer
-                </p>
+                <motion.div
+                    initial={{ scale: 2, height: "10rem" }}
+                    animate={{
+                        scale: 1,
+                        originY: 0,
+                        height: "5rem",
+                    }}
+                    transition={{
+                        duration: 1,
+                        ease: "anticipate",
+                    }}>
+                    <h1 className="text-5xl font-bold text-primary">
+                        Raazi Muhammed
+                    </h1>
+                    <p>
+                        Self-Taught Graphic Designer <br />
+                        Turned Self-Taught Web Developer
+                    </p>
+                </motion.div>
             </Container>
         </div>
     );
