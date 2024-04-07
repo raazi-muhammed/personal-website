3c3570ef2e3ee84bf9ad077be71afe59e4edcfe1
Raazi Muhammed K <raazi6163@gmail.com>
2024-04-07 19:01:04 +0530

    feat: add skill filter options

--- src/pages/homepage/Skills.tsx
+++ src/pages/homepage/Skills.tsx
@@ -1,9 +1,16 @@
-import { Card } from "@nextui-org/react";
+import { Card, Tab, Tabs } from "@nextui-org/react";
 import { AppWindowMac, Laptop, Palette } from "lucide-react";
 import Container from "../../components/layout/Components";
 import Heading from "../../components/layout/Heading";
 import { motion } from "framer-motion";
 import { animateIn } from "../../utils/animations";
+import { useState } from "react";
+
+enum SkillType {
+    TECH = "TECH",
+    INTEGRATION = "INTEGRATION",
+    TOOLS = "TOOLS",
+}
 
 export default function Skills() {
     const mainSkills: {
@@ -28,95 +35,173 @@ export default function Skills() {
             icon: <AppWindowMac size="1.5em" />,
         },
     ];
+
     const skills: {
         name: string;
+        type: SkillType;
     }[] = [
         {
             name: "Typescript",
+            type: SkillType.TECH,
         },
         {
             name: "Next.js",
+            type: SkillType.TECH,
         },
         {
             name: "Node.js",
+            type: SkillType.TECH,
         },
         {
             name: "Microservice",
+            type: SkillType.TECH,
         },
         {
             name: "Docker",
+            type: SkillType.TECH,
         },
         {
             name: "Kubernetes",
+            type: SkillType.TECH,
         },
         {
             name: "Kafka",
+            type: SkillType.TECH,
         },
         {
             name: "Express.js",
+            type: SkillType.TECH,
         },
         {
             name: "React.js",
+            type: SkillType.TECH,
         },
         {
             name: "Redux",
+            type: SkillType.TECH,
         },
         {
             name: "MongoDB",
+            type: SkillType.TECH,
         },
         {
             name: "Sequelize",
+            type: SkillType.INTEGRATION,
         },
         {
             name: "Postgress",
+            type: SkillType.TECH,
         },
         {
             name: "Git",
+            type: SkillType.TOOLS,
         },
         {
             name: "CLEAN architecture",
+            type: SkillType.TECH,
         },
         {
             name: "MVC architecture",
+            type: SkillType.TECH,
         },
         {
             name: "Swagger",
+            type: SkillType.INTEGRATION,
         },
         {
             name: "Postman",
+            type: SkillType.TOOLS,
         },
         {
             name: "Figma",
+            type: SkillType.TOOLS,
         },
         {
             name: "Illustrator",
+            type: SkillType.TOOLS,
         },
         {
             name: "Photoshop",
+            type: SkillType.TOOLS,
         },
         {
             name: "Firebase",
+            type: SkillType.INTEGRATION,
         },
         {
             name: "Tailwind",
+            type: SkillType.TECH,
         },
         {
-            name: "HTML",
+            name: "S3",
+            type: SkillType.INTEGRATION,
         },
         {
-            name: "CSS",
+            name: "Lambda",
+            type: SkillType.INTEGRATION,
         },
         {
-            name: "Javascript",
+            name: "EC2",
+            type: SkillType.INTEGRATION,
+        },
+        {
+            name: "WebSocket",
+            type: SkillType.INTEGRATION,
+        },
+        {
+            name: "Socket.IO",
+            type: SkillType.INTEGRATION,
+        },
+        {
+            name: "Razorpay",
+            type: SkillType.INTEGRATION,
+        },
+        {
+            name: "Cloudinary",
+            type: SkillType.INTEGRATION,
+        },
+        {
+            name: "SendGrid",
+            type: SkillType.INTEGRATION,
+        },
+        {
+            name: "Node Mailer",
+            type: SkillType.INTEGRATION,
+        },
+        {
+            name: "Multer",
+            type: SkillType.INTEGRATION,
+        },
+        {
+            name: "Winston",
+            type: SkillType.INTEGRATION,
+        },
+        {
+            name: "Morgan",
+            type: SkillType.INTEGRATION,
+        },
+        {
+            name: "Logstash",
+            type: SkillType.INTEGRATION,
+        },
+        {
+            name: "Kibana",
+            type: SkillType.INTEGRATION,
+        },
+        {
+            name: "Elasticsearch",
+            type: SkillType.INTEGRATION,
         },
     ];
+
+    const [skillsToShow, setSkillsToShow] = useState(skills);
 
     return (
         <section id="skills" className="py-12">
             <Container>
                 <Heading className="mb-4">Skills</Heading>
                 <div className="grid grid-cols-1 gap-4 lg:grid-cols-3">
-                    <section className="grid grid-cols-1 gap-4">
+                    <section className="grid h-fit grid-cols-1 gap-4">
                         {mainSkills.map((skill) => (
                             <motion.div
                                 initial={{
@@ -147,21 +232,49 @@ export default function Skills() {
                         ))}
                     </section>
                     <motion.section
-                        className="col-span-2 flex h-fit flex-wrap gap-4"
-                        initial="initial"
-                        whileInView="animate">
-                        {skills.map((skill, index) => (
-                            <motion.div
-                                transition={{ delay: index * 0.05 }}
-                                key={index}
-                                variants={animateIn}>
-                                <Card
-                                    radius="lg"
-                                    className="flex w-fit flex-row gap-3 rounded-large border-1 border-white/10 bg-white bg-white/10 p-2 align-middle">
-                                    <p className="mx-4">{skill.name}</p>
-                                </Card>
-                            </motion.div>
-                        ))}
+                        onViewportEnter={() => setSkillsToShow([...skills])}
+                        className="col-span-2">
+                        <Tabs
+                            defaultSelectedKey={"all"}
+                            onSelectionChange={(key) => {
+                                if (key == "all") {
+                                    setSkillsToShow(skills);
+                                    return;
+                                }
+                                setSkillsToShow(
+                                    skills.filter((s) => s.type == key)
+                                );
+                            }}
+                            size="sm"
+                            className="mx-auto mb-4 max-w-[80vw] overflow-auto"
+                            color="primary"
+                            aria-label="Tabs colors"
+                            radius="full">
+                            <Tab key="all" title="All" />
+                            <Tab key={SkillType.TECH} title="Technologies" />
+                            <Tab
+                                key={SkillType.INTEGRATION}
+                                title="Integrations"
+                            />
+                            <Tab key={SkillType.TOOLS} title="Tools" />
+                        </Tabs>
+                        <motion.section
+                            className="flex h-fit flex-wrap gap-4"
+                            initial="initial"
+                            animate="animate">
+                            {skillsToShow.map((skill, index) => (
+                                <motion.div
+                                    transition={{ delay: index * 0.05 }}
+                                    key={`${skill.name}-${new Date()}`}
+                                    variants={animateIn}>
+                                    <Card
+                                        radius="lg"
+                                        className="flex w-fit flex-row gap-3 rounded-large border-1 border-white/10 bg-white bg-white/10 p-2 align-middle">
+                                        <p className="mx-4">{skill.name}</p>
+                                    </Card>
+                                </motion.div>
+                            ))}
+                        </motion.section>
                     </motion.section>
                 </div>
             </Container>
